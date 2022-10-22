2efd070124832d62ad2985a67b7f3ae3d284b1c2
Ryan Ferguson <rferguson11@student.gsu.edu>
2022-10-21 23:52:11 -0400

    Initial commit

---
+++ Source/TowerDefense/Public/Stage1CameraActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "Stage1CameraActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API AStage1CameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
